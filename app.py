2e42e5e30c377227f76090888b05df8007e34515
pacorta00 <pacorta11@gmail.com>
2023-04-23 00:34:58 -0500

    embededLinks

--- app.py
+++ app.py
@@ -37,18 +37,18 @@ def generate_meal(restrictions):
         {'name': 'Roasted vegetable stir-fry', 'restrictions': ['vegetarian', 'vegan'], 'video_url': 'https://www.youtube.com/embed/h8IXBipqYgs'},
         {'name': 'Spaghetti with meat sauce', 'restrictions': ['nut-free'], 'video_url': 'https://www.youtube.com/embed/yfA94mLU0oo'},
         {'name': 'Carrot Soup', 'restrictions': ['gluten-free', 'nut-free', 'vegetarian'], 'video_url': 'https://www.youtube.com/embed/4coBVpdCIMQ'},
-        {'name': 'Chicken Stroganoff', 'restrictions': ['gluten-free', 'nut-free'], 'video_url': 'https://www.youtube.com/watch?v=5nGg68FnbM4&ab_channel=RecipeTinEats'},
-        {'name': 'Quick Chicken Marsala', 'restrictions': ['nut-free',], 'video_url': 'https://www.youtube.com/watch?v=_Nw3bDUpb7Q&ab_channel=NatashasKitchen'},
-        {'name': 'Chicken & Spinach Skillet Pasta with Lemon & Parmesan', 'restrictions': ['nut-free', 'gluten-free'], 'video_url': 'https://www.youtube.com/watch?v=So3MmXLYAFA&ab_channel=HealthwithDiet'},
-        {'name': 'Spinach & Mushroom Quiche', 'restrictions': ['gluten-free', 'nut-free', 'vegetarian'], 'video_url': 'https://www.youtube.com/watch?v=Dfsgi5_J2Oc&ab_channel=TastyMeals%26Treats'},
-        {'name': 'Chicken Cutlets with Sun-Dried Tomato Cream Sauce', 'restrictions': ['gluten-free', 'nut-free'], 'video_url': 'https://www.youtube.com/watch?v=C6YPhoqfHsg&ab_channel=OnesHealth'},
-        {'name': 'One-Pot Garlicky Shrimp & Broccoli', 'restrictions': ['nut-free', 'gluten-free'], 'video_url': 'https://www.youtube.com/watch?v=exuzvgnyECk&ab_channel=SoupedUpRecipes'},
-        {'name': 'Vegetable Weight-Loss Soup', 'restrictions': ['gluten-free'], 'video_url': 'https://www.youtube.com/watch?v=escHMukMphc&ab_channel=BrownGirlsKitchen'},
-        {'name': 'Shirataki Noodles with Tofu & Veggies', 'restrictions': ['nut-free', 'vegetarian'], 'video_url': 'https://www.youtube.com/watch?v=qkv_CxkdWB4&ab_channel=YeungManCooking'},
-        {'name': 'The Ultimate Vegetarian Club Sandwhich', 'restrictions': ['vegetarian', 'nut-free'], 'video_url': 'https://www.youtube.com/watch?v=j5gg365Ewaw&ab_channel=NOTANOTHERCOOKINGSHOW'},
-        {'name': 'Crispy Smashed Potatoes', 'restrictions': ['vegetarian', 'nut-free', 'gluten-free'], 'video_url': 'https://www.youtube.com/watch?v=Ai_kivZXN9o&ab_channel=NatashasKitchen'},
-        {'name': 'Creamy Linguine Spinach in a Sun dried Tomato Cream Sauce', 'restrictions': ['nut-free', 'vegetarian'], 'video_url': 'https://www.youtube.com/watch?v=3NSTpq-la3E&ab_channel=TasteDis'},
-        {'name': 'Spinach & Artichoke Dip Pasta', 'restrictions': ['nut-free', 'vegetarian'], 'video_url': 'https://www.youtube.com/watch?v=MS1eAjcp2HU&ab_channel=Mr.MakeItHappen'}
+        {'name': 'Chicken Stroganoff', 'restrictions': ['gluten-free', 'nut-free'], 'video_url': 'https://www.youtube.com/embed/5nGg68FnbM4'},
+        {'name': 'Quick Chicken Marsala', 'restrictions': ['nut-free',], 'video_url': 'https://www.youtube.com/embed/_Nw3bDUpb7Q'},
+        {'name': 'Chicken & Spinach Skillet Pasta with Lemon & Parmesan', 'restrictions': ['nut-free', 'gluten-free'], 'video_url': 'https://www.youtube.com/embed/So3MmXLYAFA'},
+        {'name': 'Spinach & Mushroom Quiche', 'restrictions': ['gluten-free', 'nut-free', 'vegetarian'], 'video_url': 'https://www.youtube.com/embed/Dfsgi5_J2Oc'},
+        {'name': 'Chicken Cutlets with Sun-Dried Tomato Cream Sauce', 'restrictions': ['gluten-free', 'nut-free'], 'video_url': 'https://www.youtube.com/embed/C6YPhoqfHsg'},
+        {'name': 'One-Pot Garlicky Shrimp & Broccoli', 'restrictions': ['nut-free', 'gluten-free'], 'video_url': 'https://www.youtube.com/embed/exuzvgnyECk?start=10'},
+        {'name': 'Vegetable Weight-Loss Soup', 'restrictions': ['gluten-free'], 'video_url': 'https://www.youtube.com/embed/escHMukMphc'},
+        {'name': 'Shirataki Noodles with Tofu & Veggies', 'restrictions': ['nut-free', 'vegetarian'], 'video_url': 'https://www.youtube.com/embed/qkv_CxkdWB4'},
+        {'name': 'The Ultimate Vegetarian Club Sandwhich', 'restrictions': ['vegetarian', 'nut-free'], 'video_url': 'https://www.youtube.com/embed/j5gg365Ewaw'},
+        {'name': 'Crispy Smashed Potatoes', 'restrictions': ['vegetarian', 'nut-free', 'gluten-free'], 'video_url': 'https://www.youtube.com/embed/Ai_kivZXN9o'},
+        {'name': 'Creamy Sun-Dried Tomato and Spinach Pasta', 'restrictions': ['nut-free', 'vegetarian'], 'video_url': 'https://www.youtube.com/embed/ll-FAFOVNVk'},#
+        {'name': 'Spinach & Artichoke Dip Pasta', 'restrictions': ['nut-free', 'vegetarian'], 'video_url': 'https://www.youtube.com/embed/MS1eAjcp2HU?start=10'}
     ]
 
     available_meals = []
